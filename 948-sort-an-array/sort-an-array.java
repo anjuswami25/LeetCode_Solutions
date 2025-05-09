f9525af7b2d4c7291502745636019f6aab979c11
Anju Swami <138124161+anjuswami25@users.noreply.github.com>
2025-05-09 23:21:43 +0530

    Time: 17 ms (94.20%) | Memory: 55.5 MB (84.90%) - LeetSync

---
+++ 948-sort-an-array/sort-an-array.java
@@ -0,0 +1,9 @@
+import java.util.Arrays;
+
+class Solution {
+    public int[] sortArray(int[] nums) {
+        // Sorting the array once
+        Arrays.sort(nums);
+        return nums;  // Return the sorted array
+    }
+}
