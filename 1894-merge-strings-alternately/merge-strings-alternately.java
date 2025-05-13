2535fa15bf7765721bc2eefd12cca9dcdaee32ed
Anju Swami <138124161+anjuswami25@users.noreply.github.com>
2025-05-13 18:19:25 +0530

    Time: 1 ms (82.18%) | Memory: 41.7 MB (71.68%) - LeetSync

--- 1894-merge-strings-alternately/merge-strings-alternately.java
+++ 1894-merge-strings-alternately/merge-strings-alternately.java
@@ -1,24 +1,19 @@
 class Solution {
     public String mergeAlternately(String word1, String word2) {
-        StringBuilder mergedString = new StringBuilder();
+        int m = word1.length();
+        int n = word2.length();
+        StringBuilder result = new StringBuilder();
         int i = 0, j = 0;
-        
-        // Merge characters alternately from both strings
-        while (i < word1.length() && j < word2.length()) {
-            mergedString.append(word1.charAt(i++));
-            mergedString.append(word2.charAt(j++));
+
+        while (i < m || j < n) {
+            if (i < m) {
+                result.append(word1.charAt(i++));
+            }
+            if (j < n) {
+                result.append(word2.charAt(j++));
+            }
         }
-        
-        // If there are remaining characters in word1
-        while (i < word1.length()) {
-            mergedString.append(word1.charAt(i++));
-        }
-        
-        // If there are remaining characters in word2
-        while (j < word2.length()) {
-            mergedString.append(word2.charAt(j++));
-        }
-        
-        return mergedString.toString();
+
+        return result.toString();
     }
 }
