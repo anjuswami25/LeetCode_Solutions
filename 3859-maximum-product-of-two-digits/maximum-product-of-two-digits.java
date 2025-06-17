fc391d3201a7518e1104a41ab35f099b1f29d1bd
Anju Swami <138124161+anjuswami25@users.noreply.github.com>
2025-06-17 08:40:46 +0530

    Time: 0 ms (100.00%) | Memory: 41.4 MB (33.99%) - LeetSync

--- 3859-maximum-product-of-two-digits/maximum-product-of-two-digits.java
+++ 3859-maximum-product-of-two-digits/maximum-product-of-two-digits.java
@@ -3,13 +3,13 @@ class Solution {
         int max1 = -1, max2 = -1;
         while (n != 0) 
         {
-            int num = n % 10;
-            n = n / 10;
+            int num = n % 10; 
+            n = n / 10; 
 
             if (max1 <= num) {
                 max2 = max1;
-                max1 = num;
-            } 
+                max1 = num;   
+            }  
             else if (num > max2) {
                 max2 = num;
             }
